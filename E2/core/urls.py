16b4180c9a8fa417433f81e147aef8711f21a5fd
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-04-18 12:38:51 -0400

    CSS

--- E2/core/urls.py
+++ E2/core/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from.views import index
-from.views import carrito
+from.views import *
 
 ## SE VAN A CREAR TODAS LAS URLS
 urlpatterns = [
     path('', index, name="index"),
     path('carrito/', carrito, name="carrito"),
+    path('compra/', compra, name="compra"),
+    path('cuenta/', cuenta, name="cuenta"),
+    path('tienda/', tienda, name="tienda"),
+    path('login/', login, name="login"),
+    path('registro/', registro, name="registro"),
+    path('producto/', producto, name="producto"),
+    path('suscripcion/', suscripcion, name="suscripcion"),
+    path('suscripcionAdmin/', suscripcionAdmin, name="suscripcionAdmin"),
+    path('voucher/', voucher, name="voucher"),
+    path('recuperarPass/', recuperarPass, name="recuperarPass"),
 ]
--- E2/core/views.py
+++ E2/core/views.py
@@ -9,3 +9,34 @@ def index(request):
 
 def carrito(request):
     return render(request, ('core/carrito.html'))
+
+def compra(request):
+    return render(request, ('core/compra.html'))
+
+def cuenta(request):
+    return render(request, ('core/cuenta.html'))
+
+def tienda(request):
+    return render(request, ('core/tienda.html'))
+
+def login(request):
+    return render(request, ('core/login.html'))
+
+def registro(request):
+    return render(request, ('core/registro.html'))
+
+def producto(request):
+    return render(request, ('core/producto.html'))
+
+def suscripcion(request):
+    return render(request, ('core/suscripcion.html'))
+
+def suscripcionAdmin(request):
+    return render(request, ('core/suscripcionAdmin.html'))
+
+def voucher(request):
+    return render(request, ('core/voucher.html'))
+
+def recuperarPass(request):
+    return render(request, ('core/recuperarPass.html'))
+
